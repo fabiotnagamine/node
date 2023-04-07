a0bb6d3be9a65d2ba4567cfac08540e9a0e650ea
fabiotnagamine <nagamine.fabio.1998@gmail.com>
2023-04-07 00:02:08 -0300

    estruturas de rep

---
+++ Do while e while/assets/js/index.js
@@ -0,0 +1,36 @@
+let indice = 1;
+
+while (indice <= 10){
+    console.log (indice);
+    indice++;
+}
+
+let nome = "Fabio";
+let i = 0
+
+while(i < nome.length){
+    console.log(nome[i]);
+    i++;
+}
+
+function random(min,max){
+    const r = Math.random() * (max - min) + min;
+    return Math.floor(r);
+}
+
+const min = 1;
+const max  = 50;
+let rand = random(min, max);
+// console.log(rand);
+
+while(rand !== 10){
+    rand = random(min, max);
+    console.log(rand);
+}
+
+console.log("#############");
+
+do {
+    rand = random(min, max);
+    console.log(rand);
+}while(rand !== 1);
--- Exercicio estrutura de repetição/assets/js/index.js
+++ Exercicio estrutura de repetição/assets/js/index.js
@@ -1,11 +1,13 @@
-const paragrafos = document.querySelector('.paragrafos');
-const ps = paragrafos.querySelectorAll('p'); 
+document.addEventListener('DOMContentLoaded', function() {
+    let paragrafos = document.querySelector('.prr');
+    const ps = paragrafos.querySelectorAll('p'); 
 
-const estilosBaby = getComputedStyle(document.body);
-const backGroundColor = estilosBaby.backgroundColor()
-
-for(let p of ps ){
-    console.log(p);
-}
-
+    const estilosBaby = getComputedStyle(document.body);
+    const backGroundColor = estilosBaby.backgroundColor;
+    
+    for (p of ps){
+        p.style.backGroundColor = backGroundColor;
+        p.style.color = 'red';
+    }
+})
  
