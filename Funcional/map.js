cd0523d8db630e0bc4bb7f420efedff378dd34d8
fabiotnagamine <nagamine.fabio.1998@gmail.com>
2024-02-28 23:57:00 -0300

    'Revisando'

--- Funcional/map.js
+++ Funcional/map.js
@@ -22,3 +22,23 @@ console.log(c);
  * Em "b" podemos ver que o valor é multiplicado por ele mesmo
  * Em "c" podemos ver que os mesmos valores de "a" foram passados para "c"
  */
+
+const pessoas = [
+    { nome: "Maria", idade: 50 },
+    { nome: "Rodrigo", idade: 35},
+    { nome: "Geovane", idade: 15},
+    { nome: "Ricardo", idade: 38}
+
+]
+
+const nomes  = pessoas.map(obj => obj.nome);
+//Usando Arrow Functions para fazer o map 
+const idades = pessoas.map(obj => ({idade: obj.idade}));
+
+const comId = pessoas.map(function(obj, index){
+    const newObj = { ...obj };
+    newObj.index = index;
+    return newObj;
+})
+
+console.log(comId)
--- Funcional/reduce.js
+++ Funcional/reduce.js
@@ -3,9 +3,20 @@
  * especificada para produzir um unicoo valor.
  */
 
-let a = [1,2,3,4,5]
+let a = [1,2,3,4,5,545, 468, 467865, 2646, 56456, 222223, 5569415]
 let sum = a.reduce(function(x,y) { return x+y }, 0); // Soma de valores
 let product = a.reduce(function(x,y) { return x*y }, 1); // Produto de valores
 let max = a.reduce(function(x,y) { return (x>y)?x:y; }); // Maior valor
 
 console.log(sum, product, max);
+
+const total = a.reduce(function(acumulador, valor, indice, array){
+    // acumulador += valor;
+    // return acumulador;
+    if(valor % 63 == 0) {
+        acumulador.push(valor)
+    }
+    return acumulador;
+},[]);
+
+console.log("Total " + total);
