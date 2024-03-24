cba683945b7c2e5a814ef97ab7654a4195df6b0d
fabiotnagamine <nagamine.fabio.1998@gmail.com>
2024-03-24 20:04:44 -0300

    polimorfismo

--- Herança/Exercicio/Validação CPF/index.js
+++ Herança/Exercicio/Validação CPF/index.js
@@ -9,23 +9,33 @@ ValidaCPF.prototype.criaDigito = function (cpfParcial) {
     const cpfArray = Array.from(cpfParcial);
 
     let regressivo = cpfArray.length + 1;
-    const digito = cpfArray.reduce((ac, val) => {
-        console.log(regressivo);
+    const total = cpfArray.reduce((ac, val) => {
+        // console.log(regressivo, val, regressivo * val);
+        ac += (regressivo * Number(val));
         regressivo--;
         return ac;
     }, 0);
+    
+    const digitos = 11 - (total % 11);
+    return digitos > 9 ? '0' : String(digitos);
 }
 
 ValidaCPF.prototype.valida = function () {
     if (typeof this.cpfLimpo === 'undefined') return false;
     if (this.cpfLimpo.length !== 11) return false;
+    if (this.isSequencia(this.cpfLimpo)) return false;
 
     const cpfParcial = this.cpfLimpo.slice(0, -2);
     const digito1 = this.criaDigito(cpfParcial);
+    
 
     return true;
 }
-const cpf = new ValidaCPF('123.456.789-10');
+ValidaCPF.prototype.isSequencia = function(){
+    const sequencia =  this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+    return sequencia === this.cpfLimpo;
+}
+const cpf = new ValidaCPF('111.111.111-11');
 console.log(cpf.valida());
 
 
