45c3b30cb50f29fc31dc67065b1c0bd3867029a6
fabiotnagamine <nagamine.fabio.1998@gmail.com>
2024-04-28 23:31:16 -0300

    'webpack'

---
+++ exportModules/public/assets/js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************************!*\
+  !*** ./src/assets/js/index.js ***!
+  \********************************/
+var publico = 12;
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ webpack_/src/assets/js/index.js
@@ -0,0 +1,1 @@
+const publico = 12;
